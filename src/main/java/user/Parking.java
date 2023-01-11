2c997bc0dbe99a8665264e7bdd474edb07211210
nevds1d <scottsdale787@gmail.com>
2023-01-11 18:03:59 +0500

    add method reserveParkingPaces

--- src/main/java/user/Parking.java
+++ src/main/java/user/Parking.java
@@ -1,2 +1,29 @@
-package user;public class Parking {
+package user;
+
+public class Parking {
+    public int freeParkingPlaces = 100;
+    public boolean isFull = true;
+
+    // метод, который выводит количество свободных мест
+    public void printFreeParkingPlacesAmount() {
+        System.out.println("Свободных парковочных мест осталось " + freeParkingPlaces);
+    }
+    // метод, который резервирует парковочное место
+    public void reserveParkingPlace() {
+        if(!isFull) {
+            // если парковка не заполнена, можно забронировать место;
+            // иначе нужно вывести сообщение о том, что мест больше нет
+            System.out.println("Парковочное место забронированно");
+            freeParkingPlaces--;
+            printFreeParkingPlacesAmount();
+            // если после бронирования не осталось свободных мест,
+            // то парковка заполнена: переменная isFull == true
+            if(freeParkingPlaces == 0){
+                isFull = true;
+            }
+        } else {
+            System.out.println("Свободных мест нет!");
+        }
+    }
+
 }
